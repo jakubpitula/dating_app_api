d2126df8022651678467dbf3012e9b19d1eb9782
kuba <szachmistrz2@gmail.com>
2022-11-11 14:21:35 +0000

    Authentication

--- main.py
+++ main.py
@@ -23,11 +23,47 @@ app.add_middleware(
     allow_headers=allow_all
 )
 
+
 # signup endpoint
+@app.post("/signup")
+async def signup(request: Request):
+    req = await request.json()
+    email = req['email']
+    password = req['password']
+    if email is None or password is None:
+        return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return JSONResponse(content={'message': f'Successfully created user {user.uid}'}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'Error Creating User'}, status_code=400)
+
 
 # login endpoint
+@app.post("/login")
+async def login(request: Request):
+    req_json = await request.json()
+    email = req_json['email']
+    password = req_json['password']
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return JSONResponse(content={'token': jwt}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
 
-# ping endpoint
+
+# validate endpoint
+@app.post("/validate")
+async def validate(request: Request):
+    headers = request.headers
+    jwt = headers.get('authorization')
+    user = auth.verify_id_token(jwt)
+    return user["uid"]
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
