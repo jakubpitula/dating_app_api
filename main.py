336cff1c563c2eb4737a394a3e30de0c836e7845
kuba <szachmistrz2@gmail.com>
2023-01-31 22:52:20 +0000

    updating users done

--- main.py
+++ main.py
@@ -125,10 +125,9 @@ async def get_user(request: Request):
     return JSONResponse(content=ref.child("users").child(uid).get(), status_code=200)
 
 
-@app.put("/user/{id}")
+@app.put("/user/{uid}")
 async def update_user(uid: str, request: Request):
-
-    # todo:constraints on user data, update auth data
+    # todo:constraints on user data
 
     headers = request.headers
     jwt = headers.get('authorization')
@@ -142,16 +141,28 @@ async def update_user(uid: str, request: Request):
         return JSONResponse(content={'msg': 'Not authorized'}, status_code=401)
 
     req_json = await request.json()
+    try:
+        auth.update_user(
+            uid,
+            email=req_json["email"] if "email" in req_json else auth.get_user(uid).email,
+            display_name=req_json["name"] if "name" in req_json else auth.get_user(uid).display_name
+        )
+    except:
+        return JSONResponse(content={'msg': 'Incorrect data'}, status_code=400)
+
     user = ref.child("users").child(uid)
 
-    if "email" in req_json:
-        user.update({"email": req_json["email"]})
-    if "age" in req_json:
-        user.update({"age": req_json["age"]})
-    if "gender" in req_json:
-        user.update({"gender": req_json["gender"]})
-    if "name" in req_json:
-        user.update({"name": req_json["name"]})
+    try:
+        if "email" in req_json:
+            user.update({"email": req_json["email"]})
+        if "age" in req_json:
+            user.update({"age": req_json["age"]})
+        if "gender" in req_json and req_json["gender"] in ('m', 'f', 'nb', 'pns'):
+            user.update({"gender": req_json["gender"]})
+        if "name" in req_json:
+            user.update({"name": req_json["name"]})
+    except:
+        return JSONResponse(content={"msg": "Error updating user"}, status_code=500)
 
     return JSONResponse(content=user.get(), status_code=200)
 
